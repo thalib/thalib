d0200558255cf3ca0a96c263eaa1a000d348846b
Mohamed Thalib H <hmthalib@gmail.com>
2016-10-27 18:29:58 +0530

    Updated script envsetup.sh
    Signed-off-by: Mohamed Thalib H <hmthalib@gmail.com>

--- script/evnsetup.sh
+++ script/evnsetup.sh
@@ -6,22 +6,26 @@ POST_TIME=`date +%H:%M:%S`
 POST_TITLE="
 ---
 layout: post
-title:  \"Wonderfull article\"
+title:  \"Wonderfull article (CHANGE ME)\"
 date:   $POST_DATE $POST_TIME
 categories: General
 tags: 
-excerpt: This post is all about XYZ
+excerpt: This post is all about XYZ (CHANGE ME)
 ---
 
 
 "
-POST_NAME=$POST_PATH/$POST_DATE-article-$RANDOM.md
 
 function post-create() {
+	POST_NAME=$POST_PATH/$POST_DATE-NEW-POST-CHANGE-MY-NAME-$RANDOM.md
 	echo "$POST_TITLE" > $POST_NAME
 	echo "Created $POST_NAME"
 }
 
 function post-cd() {
 	cd $POST_PATH
+}
+
+function post-clean() {
+	rm `find . | grep '~'`
 }
